6649417c02c743617fbc5641ef26db32197e0650
Ruohuang Zheng <zhengruohuang@gmail.com>
2019-12-25 14:28:06 -0500

    Initial workflow

--- rtl/revolve.v
+++ rtl/revolve.v
@@ -6,10 +6,11 @@ module revolve (
 
 reg [31:0] counter;
 
+
 always @(posedge clk or negedge rst_n) begin
     if (rst_n == 1'b0) begin
         counter <= 32'b0;
-    end else if (clk) begin
+    end else begin
         counter <= counter + 32'b1;
     end
 end
